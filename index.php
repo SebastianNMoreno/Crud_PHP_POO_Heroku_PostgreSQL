5064c3617e8190009267a6cd6b3daf81bd3ac305
SebastianNMoreno <38090268+SebastianNMoreno@users.noreply.github.com>
2018-04-14 16:21:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 
     <div class="ui small circular rotate left reveal image imgcenter">
         <img src="imagenes/heroku.png" class="visible content ">
-        <img src="imagenes/1200px-Postgresql_elephant.svg.png" class="hidden content">
+        <img src="imagenes/mifoto.jpg" class="hidden content">
     </div>
 <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
 
